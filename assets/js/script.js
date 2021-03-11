24ae8405f8eb555013050236c3338cf67c769198
jbeedle19 <josh.beedle@gmail.com>
2021-03-11 13:14:47 -0500

    finished offline functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 require('bootstrap');
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+const img1 = require("../../dist/assets/img/food-table.jpg");
+const img2 = require("../../dist/assets/img/grill.jpg");
 
 $(document).ready(function() {
-
   // First image is hard coded in index.html
   const carouselSlides = [
     {
